d7310a3cf3bcb3642c55aa7a54f46855a97c6993
Tim B <41713011+TBosak@users.noreply.github.com>
2018-10-10 21:25:37 -0500

    Added browser options
    Added support for Chrome, Edge, and Safari.

--- bin/giftdigger.rb
+++ bin/giftdigger.rb
@@ -4,11 +4,31 @@ require 'webdrivers'
 require_relative 'botbones.rb'
 require 'httparty'
 
-foxy = Watir::Browser.new :firefox, profile: 'default'
+gg = 0
+while gg == 0 do
+puts "Enter a number for your browser of choice: 1. Firefox, 2. Chrome, 3. Edge, 4. Safari"
+browseopt = gets.to_i
+puts "Please wait while your browser of choice is loaded."
+if browseopt == 1
+foxy = Watir::Browser.new :firefox
+gg = 1
+elsif browseopt == 2
+foxy = Watir::Browser.new :chrome
+gg = 1
+elsif browseopt == 3
+foxy = Watir::Browser.new :edge
+gg = 1
+elsif browseopt == 4
+foxy = Watir::Browser.new :safari, technology_preview: true
+gg = 1
+else
+puts "Selection is invalid, try again."
+end
+end
 url = "www.steamgifts.com"
 @home = URI(url)
 foxy.goto(@home)
-puts "After logging in, press any key to continue."
+puts "After logging in, press enter to continue."
 confirm = gets
 puts "What term would you like to search for in the forum? 'train' is a really good option!"
 term = gets
@@ -19,7 +39,6 @@ url = "www.steamgifts.com/discussions/search?q=#{term}"
 foxy.goto(@place)
 @links = foxy.links.collect(&:href)
 @finlinks = @links.uniq.select{|x| x.match("https://www.steamgifts.com/discussion/.*")}
-
 num = 0
 while num < @digamount do
 @finlinks.each do |x|
